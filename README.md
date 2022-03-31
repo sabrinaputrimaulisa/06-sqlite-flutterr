62408540d4e4ed8c68f800ac7d1eab668b251336
sabrinaputrimaulisa <sabrinaputrimaulisa@gmail.com>
2022-04-01 00:13:03 +0700

    tugas_6

---
+++ README.md
@@ -0,0 +1,15 @@
+# Tugas 6 Pemrograman Mobile (DATABASE SQLITE)
+
+Nama  : Sabrina Putri Maulisa <br/>
+NIM   : 2031710022 <br/>
+Kelas : MI-2C <br/>
+Prodi : D3 MI <br/>
+
+## Hasil Praktikum
+
+<img src="img/a.png" alt="drawing" width="300"/>
+<br/>
+
+<img src="img/b.png" alt="drawing" width="300"/>
+<br/>
+
---
+++ lib/home.dart
@@ -0,0 +1,133 @@
+//MI2F_14_M. Thosin Yuhaililul Hilmi_2031710032
+
+// ignore_for_file: deprecated_member_use
+
+import 'package:flutter/material.dart';
+import 'package:sqflite/sqflite.dart';
+import 'dart:async';
+import 'dbhelper.dart';
+import 'entryform.dart';
+import 'item.dart';
+
+//pendukung program asinkron
+class Home extends StatefulWidget {
+  @override
+  HomeState createState() => HomeState();
+}
+
+class HomeState extends State<Home> {
+  DbHelper dbHelper = DbHelper();
+  int count = 0;
+  List<Item> itemList;
+  @override
+  Widget build(BuildContext context) {
+    if (itemList == null) {
+      itemList = List<Item>();
+    }
+    return Scaffold(
+      appBar: AppBar(
+        title:
+            const Text('Daftar Item - SABRINA PUTRI MAULISA | 2031710022'),
+      ),
+      body: Column(children: [
+        Expanded(
+          child: createListView(),
+        ),
+        Container(
+          alignment: Alignment.bottomCenter,
+          child: SizedBox(
+            width: double.infinity,
+            child: RaisedButton(
+              child: const Text("Tambah Item"),
+              onPressed: () async {
+                var item = await navigateToEntryForm(context, null);
+                if (item != null) {
+                  //TODO 2 Panggil Fungsi untuk Insert ke DB
+                  int result = await dbHelper.insert(item);
+                  if (result > 0) {
+                    updateListView();
+                  }
+                }
+              },
+            ),
+          ),
+        ),
+      ]),
+    );
+  }
+
+  Future<Item> navigateToEntryForm(BuildContext context, Item item) async {
+    var result = await Navigator.push(context,
+        MaterialPageRoute(builder: (BuildContext context) {
+      return EntryForm(item);
+    }));
+    return result;
+  }
+
+  ListView createListView() {
+    TextStyle textStyle = Theme.of(context).textTheme.headline5;
+    return ListView.builder(
+      itemCount: count,
+      itemBuilder: (BuildContext context, int index) {
+        return Card(
+          color: Colors.white,
+          elevation: 2.0,
+          child: ListTile(
+            leading: const CircleAvatar(
+              backgroundColor: Colors.red,
+              child: Icon(Icons.ad_units),
+            ),
+            title: Text(
+              itemList[index].name,
+              style: textStyle,
+            ),
+            subtitle: Text(this.itemList[index].price.toString()),
+            trailing: GestureDetector(
+              child: const Icon(Icons.delete),
+              onTap: () {
+                //TODO 3 Panggil Fungsi untuk Delete data
+                deleteItem(itemList[index]);
+              },
+            ),
+            onTap: () async {
+              var item = await navigateToEntryForm(context, itemList[index]);
+              //TODO 4 Panggil Fungsi untuk Edit data
+              editItem(itemList[index]);
+            },
+          ),
+        );
+      },
+    );
+  }
+
+  //update List item
+  void updateListView() {
+    final Future<Database> dbFuture = dbHelper.initDb();
+    dbFuture.then((database) {
+      //TODO 1 Select data dari DB
+      Future<List<Item>> itemListFuture = dbHelper.getItemList();
+      itemListFuture.then((itemList) {
+        setState(() {
+          this.itemList = itemList;
+          count = itemList.length;
+        });
+      });
+    });
+  }
+
+  //delete contact
+  void deleteItem(Item object) async {
+    int result = await dbHelper.delete(object.id);
+    if (result > 0) {
+      updateListView();
+    }
+  }
+
+  //Edit contact
+  void editItem(Item object) async {
+    int result = await dbHelper.update(object);
+    if (result > 0) {
+      updateListView();
+    }
+  }
+}
